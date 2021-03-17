fd5d3013f06cf44aae83c47b0d1d42a2d7bdeb6a
William Sgobi <william_sgobi@hotmail.com>
2021-03-17 16:15:29 -0300

    primeiro modelview, controller Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+
+            list.Add(new Department(1, "Eletronics"));
+            list.Add(new Department(2, "Fashion"));
+            list.Add(new Department(3, "Home"));
+
+            return View(list);
+        }
+    }
+}
